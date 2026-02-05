211b87e8d60d2ec6a2afbd8bbecaa902920ee00c
hendri2212 <cowok_cool320@yahoo.co.id>
2026-02-05 19:23:24 +0800

    modify templating

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,35 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Absensi Guru')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <h1>Absensi Guru</h1>
+        <nav>
+            <a href="/">Home</a>
+            @guest
+                <a href="/login">Login</a>
+            @else
+                <form action="/logout" method="POST" style="display: inline;">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
+            @endguest
+            <a href="/absen">Absensi</a>
+            <a href="/nilai">Nilai</a>
+            <a href="/data">Data</a>
+            <a href="/dashboard">Dashboard</a>
+        </nav>
+        <hr>
+    </header>
 
-@section('title', 'Beranda')
-
-@section('content')
-    <h2>Selamat Datang</h2>
-    <p>Silakan pilih menu di atas untuk melanjutkan.</p>
-@endsection
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
