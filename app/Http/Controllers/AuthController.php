163fe71691c7730468b991205626ac865eb7e2fb
reza-sketsa <rrezaalfarisi@gmail.com>
2026-05-23 18:31:43 +0800

    typo

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,7 +9,7 @@ class AuthController extends Controller
 {
     public function showLogin()
     {
-        return view('auth.login');
+        return view('Auth.login');
     }
 
     public function login(Request $request)
